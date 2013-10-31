97c68c5c393530bd02a056f19cecbdad1020f7b2
woutersioen <wouter.sioen@wijs.be>
2013-10-31 08:04:51 +0100

    Initial commit

---
+++ Converter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class Converter
+ *
+ * An Sir Trevor to HTML conversion helper for PHP
+ *
+ * @version 1.0.0
+ * @author Wouter Sioen <info@woutersioen.be>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+class Converter
+{
+
+}
