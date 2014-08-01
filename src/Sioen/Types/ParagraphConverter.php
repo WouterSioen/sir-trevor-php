5df38b44787e7830fb340bd519eee8c2d4753671
WouterSioen <wouter.sioen@gmail.com>
2014-08-01 19:01:57 +0200

    Let the ParagraphConverter fallback to the BaseConverter.

--- src/Sioen/Types/ParagraphConverter.php
+++ src/Sioen/Types/ParagraphConverter.php
@@ -4,15 +4,4 @@ namespace Sioen\Types;
 
 class ParagraphConverter extends BaseConverter implements ConverterInterface
 {
-    public function toJson(\DOMElement $node)
-    {
-        $html = $node->ownerDocument->saveXML($node);
-
-        return array(
-            'type' => 'text',
-            'data' => array(
-                'text' => ' ' . $this->htmlToMarkdown($html)
-            )
-        );
-    }
 }
