92d06890ce437c58535e688b5e58f591184d4bd2
woutersioen <wouter.sioen@gmail.com>
2016-04-05 16:44:45 +0200

    Avoid two levels of indentation.
    This also extracts some functionality in methods with a more
    comprehensive and explicit naming.

--- src/Sioen/HtmlToJson/BlockquoteConverter.php
+++ src/Sioen/HtmlToJson/BlockquoteConverter.php
@@ -8,18 +8,7 @@ final class BlockquoteConverter implements Converter
 
     public function toJson(\DOMElement $node)
     {
-        // check if the quote contains a cite
-        $cite = '';
-
-        foreach ($node->childNodes as $child) {
-            // if it contains a 'cite' node, we should add it in the cite property
-            if ($child->nodeName == 'cite') {
-                $html = $child->ownerDocument->saveXML($child);
-                $html = preg_replace('/<(\/|)cite>/i', '', $html);
-                $child->parentNode->removeChild($child);
-                $cite = ' ' . $this->htmlToMarkdown($html);
-            }
-        }
+        $cite = $this->getCiteHtml($node);
 
         // we use the remaining html to create the remaining text
         $html = $node->ownerDocument->saveXML($node);
@@ -37,5 +26,29 @@ final class BlockquoteConverter implements Converter
     public function matches(\DomElement $node)
     {
         return $node->nodeName === 'blockquote';
+    }
+
+    private function getCiteHtml(\DOMElement $node)
+    {
+        $cite = '';
+
+        foreach ($this->getCiteNodes($node) as $child) {
+            $html = $child->ownerDocument->saveXML($child);
+            $html = preg_replace('/<(\/|)cite>/i', '', $html);
+            $child->parentNode->removeChild($child);
+            $cite = ' ' . $this->htmlToMarkdown($html);
+        }
+
+        return $cite;
+    }
+
+    private function getCiteNodes(\DOMElement $node)
+    {
+        return array_filter(
+            iterator_to_array($node->childNodes),
+            function (\DOMElement $childNode) {
+                return $childNode->nodeName == 'cite';
+            }
+        );
     }
 }
